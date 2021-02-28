286159afd9e8486a64053fd30a9b1b4cf5103ec4
ziofil <ziofil@gmail.com>
2021-02-28 18:31:30 +0100

    first faint steps

--- src/rs485_master.py
+++ src/rs485_master.py
@@ -19,6 +19,8 @@ import tornado.ioloop         # pylint: disable=import-error
 import tornado.websocket      # pylint: disable=import-error
 import tornado.options        # pylint: disable=import-error
 
+import json
+
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 LISTEN_PORT = 8000
@@ -57,6 +59,7 @@ class HttpHandler(tornado.web.RequestHandler):  # pylint: disable=too-few-public
 
 class WebsockHandler(tornado.websocket.WebSocketHandler):
 
+
     def initialize(self):
 
         a = get_application_instance()
@@ -65,6 +68,7 @@ class WebsockHandler(tornado.websocket.WebSocketHandler):
         logging.info(f"n. of active web_socket_channels:{len(a.web_socket_channels)}")
 
         self.msg_counter = 0
+        self.RS485_instance = rs485_Master()
 
     def open(self, *args, **kwargs):
 
@@ -74,14 +78,19 @@ class WebsockHandler(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
 
         logging.info(f"message:{message}")
-        self.msg_counter += 1
-        answ = {
-            'time': time.asctime(),
-            'js': 'document.getElementById("answer_target").innerHTML="{} [{}] {}";'.format(
-                self.msg_counter, time.asctime(), message),
-        }
+        try:
+            in_message = json.loads(message)
+            logging.info(in_message)
+            self.msg_counter += 1
+            cmd_name = in_message["command_name"]
+            cmd_args = in_message["arguments"]
+            inst = get_application_instance().rs485_instance
+            answ = {"answer_name": cmd_name, "answer_content": inst.process_command(cmd_name, cmd_args)}
+            self.write_message(answ)
+        except Exception as e:
+            logging.info(f"failed to decode message ", e)
+            
 
-        self.write_message(answ)
 
     def on_close(self):
 
@@ -91,15 +100,46 @@ class WebsockHandler(tornado.websocket.WebSocketHandler):
 
 
 class rs485_Master:
+    def start_backend_task(self):
+
+        logging.info("starting backend task...")
+
+        _future = self.run()
+        asyncio.ensure_future(_future)
+        
+    def __init__(self):
+        logging.info("RS485_Master init ...")
+        self.start_backend_task()
+
+
+    def connect(self, arguments):
+        return True
+
+    def disconnect(self, arguments):
+        return True
+
+    def sendOnSerial(self, arguments):
+        return True
+
+    def process_command(self, command_name, arguments):
+        commands_ = {
+           "connect": rs485_Master.connect,
+           "disconnect": rs485_Master.disconnect,
+           "send_on_serial": rs485_Master.disconnect,
+        }
+
+        return commands_[command_name](self, arguments)
+
 
     async def run(self):
 
         msg_ = """ TO BE IMPLEMENTED ..."""
 
         while True:
+                await asyncio.sleep(1)
+                logging.warning(msg_)
 
-            await asyncio.sleep(1)
-            logging.warning(msg_)
+
 
 
 class Application:
@@ -119,17 +159,10 @@ class Application:
         app.listen(LISTEN_PORT, LISTEN_ADDRESS)
         tornado.platform.asyncio.AsyncIOMainLoop().install()
 
-    def start_backend_task(self):
 
-        logging.info("starting backend task...")
-
-        _future = rs485_Master().run()
-        asyncio.ensure_future(_future)
 
     def run(self):
-
         self.start_tornado()
-        self.start_backend_task()
 
         asyncio.get_event_loop().run_forever()
 
@@ -142,3 +175,7 @@ def main():
 
     a = get_application_instance()
     a.run()
+
+
+if __name__ == '__main__':
+    main()
